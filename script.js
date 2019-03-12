416e45a983e549fecea99c7de8b7e1efa208178b
hyuk5209 <48271363+hyuk5209@users.noreply.github.com>
2019-03-12 15:42:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var Color = {
+    toNight: function(){
+        document.querySelector('body').style.backgroundColor = 'black';
+        document.querySelector('body').style.color = 'white';   
+        var links = document.querySelector('a');
+        var i = 0;
+            while(i < links.length){
+                links[i].style.color='white';
+                i= i+1;
+    }
+    },
+    toDay: function(){
+        document.querySelector('body').style.backgroundColor = 'white';
+        document.querySelector('body').style.color = 'black'; 
+        var links = document.querySelector('a');
+        var i = 0;
+            while(i < links.length){
+                links[i].style.color='black';
+                i= i+1;
+    }
+}
